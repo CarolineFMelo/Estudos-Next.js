9d9d9b69ac0f1f0d5c704d7862a5970c21f53a4d
carolineferreiramelo <caroline.melo@soitic.com>
2021-11-18 08:33:19 -0300

    first commit

---
+++ src/pages/profile/[id].js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import router, { useRouter } from 'next/router';
+
+function Profile({ user = {} }) {
+    const router = useRouter();
+
+    if(router.isFallback) return <h1>Carregando...</h1>;
+
+    return (
+        <div>
+            <p>{user.id}</p>
+            <p>{user.name}</p>
+            <p>{user.username}</p>
+        </div>
+    );
+}
+
+export async function getStaticProps(context) {
+    const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/users', 
+        { params: { id: context.params.id } }
+    );
+    const user = await response.data[0];
+
+    // await new Promise((res) => setTimeout(res, 4000));
+
+    return {
+        props: { user, revalidade: 1 },
+    };
+}
+
+export async function getStaticPaths() {
+    const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/users'
+    );
+    const users = await response.data.slice(0, 5);
+
+    const paths = users.map((user) => {
+        return { params: { id: String(user.id) } };
+    });
+    return {
+        paths,
+        fallback: true,
+    };
+}
+
+export default Profile;
---
+++ src/pages/users.js
@@ -0,0 +1,45 @@
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+import Link from 'next/link';
+
+function Users({users}) {
+    // const [users, setUsers] = useState([]);
+
+    // const fetchUsers = async () => {
+    //     const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+    //     const data = await response.data;
+    //     setUsers(data);
+    // };
+
+    // console.log(users);
+    
+    // useEffect(() => {
+    //     fetchUsers();
+    // }, []);
+    
+    return (
+        <div>
+            {users.map((user) =>(
+                <div>
+                    <link href="/profile/[id]" 
+                          as={`/profile/${user.id}`}>
+                        <a>{user.name}</a>
+                    </link>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export async function getStaticProps(context) {
+    const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/users'
+    );
+    const data = await response.data;
+
+    return {
+        props: { users: data },
+    };
+}
+
+export default Users;
